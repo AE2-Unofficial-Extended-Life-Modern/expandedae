b4f236a44c0d0b43b2635d19ddd1ab03bbf15f4a
Kolja <contact@kolja.lu>
2026-09-13 17:07:01 +0200

    fix crash with latest version of extendedae
    Signed-off-by: Kolja <contact@kolja.lu>

--- src/main/java/lu/kolja/expandedae/xmod/extendedae/ExtendedAE.java
+++ src/main/java/lu/kolja/expandedae/xmod/extendedae/ExtendedAE.java
@@ -3,12 +3,19 @@ package lu.kolja.expandedae.xmod.extendedae;
 import appeng.api.upgrades.Upgrades;
 import appeng.core.definitions.ItemDefinition;
 import com.glodblock.github.extendedae.common.EPPItemAndBlock;
-import com.glodblock.github.extendedae.common.items.ItemMEPackingTape;
+import com.glodblock.github.extendedae.config.EPPConfig;
+import lu.kolja.expandedae.Expandedae;
 import lu.kolja.expandedae.definition.ExpBlocks;
 import lu.kolja.expandedae.definition.ExpItems;
 import lu.kolja.expandedae.item.upgrade.Ext2ExpUpgradeItem;
 import lu.kolja.expandedae.item.upgrade.Ext2GigaUpgradeItem;
 
+import net.minecraftforge.eventbus.api.EventPriority;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.config.ModConfigEvent;
+
+@Mod.EventBusSubscriber(modid = Expandedae.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ExtendedAE {
     public static ItemDefinition<Ext2ExpUpgradeItem> EXT_PATTERN_PROVIDER_UPGRADE;
     public static ItemDefinition<Ext2GigaUpgradeItem> EXT2G_PATTERN_PROVIDER_UPGRADE;
@@ -19,10 +26,6 @@ public class ExtendedAE {
     }
 
     public ExtendedAE() {
-        ItemMEPackingTape.registerPackableDevice(ExpBlocks.EXP_PATTERN_PROVIDER.id());
-        ItemMEPackingTape.registerPackableDevice(ExpItems.EXP_PATTERN_PROVIDER_PART.id());
-        ItemMEPackingTape.registerPackableDevice(ExpBlocks.EXP_IO_PORT.id());
-
         Upgrades.add(ExpItems.AUTO_COMPLETE_CARD, EPPItemAndBlock.EX_PATTERN_PROVIDER, 1, "group.ex_pattern_provider.name");
         Upgrades.add(ExpItems.AUTO_COMPLETE_CARD, EPPItemAndBlock.EX_PATTERN_PROVIDER_PART, 1, "group.ex_pattern_provider.name");
         /*
@@ -35,5 +38,16 @@ public class ExtendedAE {
         Upgrades.add(ExpItems.STICKY_CARD, EPPItemAndBlock.MOD_STORAGE_BUS,1, "group.mod_storage_bus.name");
         Upgrades.add(ExpItems.STICKY_CARD, EPPItemAndBlock.PRECISE_STORAGE_BUS,1, "group.precise_storage_bus.name");
         */
+    }
+
+    @SubscribeEvent(priority = EventPriority.LOWEST)
+    public static void onConfigLoad(ModConfigEvent event) {
+        if (!event.getConfig().getModId().equals("expatternprovider")) {
+            return;
+        }
+
+        EPPConfig.tapeWhitelist.add(ExpBlocks.EXP_PATTERN_PROVIDER.id());
+        EPPConfig.tapeWhitelist.add(ExpItems.EXP_PATTERN_PROVIDER_PART.id());
+        EPPConfig.tapeWhitelist.add(ExpBlocks.EXP_IO_PORT.id());
     }
 }
